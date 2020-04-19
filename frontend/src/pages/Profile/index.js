ab7ad066ab2c01150fcd340669bc79e23c3852b8
Rudolf Jr <rudinhok@gmail.com>
2020-04-18 21:49:45 -0300

    validar se usuário esta logado ou não

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,18 +11,24 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
 
     const [incidents, setIncidents] = useState([]);
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
     useEffect(() => {
+
+        if(!ongId){
+            history.push('/');
+        }
+
         api.get('profile', {
             headers: {
                 Authorization: ongId,
             }
         }).then(response => {
             setIncidents(response.data);
-        })
+        });
     }, [ongId]);
 
     async function handleDeleteIncident(id){
@@ -39,6 +45,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -46,7 +57,7 @@ export default function Profile() {
                 <span>Bem vindo, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
